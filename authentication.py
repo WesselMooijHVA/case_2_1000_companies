f1234b73f06c78f7a953deb82fad4c8d6fbc9be8
Jarifa <39035187+Jarifa@users.noreply.github.com>
2022-09-28 13:17:07 +0200

    update

--- authentication.py
+++ authentication.py
@@ -1,11 +1,9 @@
-def auth():
+def auth(api):
   #imports
-  from kaggle.api.kaggle_api_extended import KaggleApi
   import json
   import os
 
   #initiating API and setting username and key as environment variables
-  api = KaggleApi()
   with open('kaggle.json') as json_file:
     config_data = json.load(json_file)
   os.environ['KAGGLE_USERNAME'] = config_data['username']
