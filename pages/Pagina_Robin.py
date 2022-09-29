c94d7985ed5c6e12f3dd1ecc78fa95c7c20583ee
Jarifa <39035187+Jarifa@users.noreply.github.com>
2022-09-29 15:59:51 +0200

    Update Pagina_Robin.py

--- pages/Pagina_Robin.py
+++ pages/Pagina_Robin.py
@@ -116,8 +116,7 @@ fig.update_layout(height=600, width=1000, title_text="Side By Side 2021 & 2022")
 st.plotly_chart(fig)
 
 st.subheader('de code')
-code= '''
-#build slider
+plot_code = '''#build slider
 selectie = st.slider(
     'Selecteer het aantal bedrijven (Top)',0,int(df_2022.shape[0]),10)
 
@@ -158,6 +157,6 @@ fig.update_layout(height=600, width=1000, title_text="Side By Side 2021 & 2022")
 
 st.plotly_chart(fig)
 '''
-st.code(code)
+st.code(plot_code)
 
 
