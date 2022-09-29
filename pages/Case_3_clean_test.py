b97f2e4249bcf2c0f9ce586c370b95cf0bb9e79b
WesselMooijHVA <114337239+WesselMooijHVA@users.noreply.github.com>
2022-09-29 14:13:55 +0200

    Update Case_3_clean_test.py

--- pages/Case_3_clean_test.py
+++ pages/Case_3_clean_test.py
@@ -171,7 +171,7 @@ df_2022.fillna(0, axis= 1)''''', language= 'python')
 # In[24]:
 
 
-df_2022['Market_Value_change']= df_2022['Market Value 2022'] - df_2022['Market Value 2021']
+df_2022['Market_Value_change']= df_2022['Market Value 2022'] - df_2022['Market Value 2021']*1000
 df_2022['profit_per_employee_2021']= df_2022['Profit 2021']/ df_2022['Employees'] 
 df_2022['profit_per_employee_2022']= df_2022['Profits 2022']/ df_2022['Employees']
 df_2022['revenue_per_employee_2022']= df_2022['Revenues 2022']/ df_2022['Employees']
